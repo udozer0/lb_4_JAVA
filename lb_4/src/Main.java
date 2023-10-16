51ef31632ab23f51d64dbb61baf96294f2ecc635
Ivan-Zelyapugin <zeluapugini@inbox.ru>
2023-10-16 22:51:57 +0300

    message

--- lb_4/src/Main.java
+++ lb_4/src/Main.java
@@ -11,8 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.print("тяжело");
-            System.out.print("я устал");
+            System.out.print("пошли в танки");
             System.out.print("Введите первую строку: ");
             String firstLine = scanner.nextLine();
             System.out.print("Введите вторую строку: ");
