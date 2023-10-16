dc4837f1d69c575205713031d203392751875699
Ivan-Zelyapugin <zeluapugini@inbox.ru>
2023-10-16 21:57:14 +0300

    qw

--- lb_4/src/Main.java
+++ lb_4/src/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-
-            System.out.print("ВСё четко");
-
+            System.out.print("тяжело");
             System.out.print("Введите первую строку: ");
             String firstLine = scanner.nextLine();
             System.out.print("Введите вторую строку: ");
