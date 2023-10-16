af388950ff1985420611714ce0a5d270b780cdca
Udozer <maxim228.karin@gmail.com>
2023-10-16 21:59:28 +0300

    я устал

--- lb_4/src/Main.java
+++ lb_4/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             System.out.print("тяжело");
+            System.out.print("я устал");
             System.out.print("Введите первую строку: ");
             String firstLine = scanner.nextLine();
             System.out.print("Введите вторую строку: ");
